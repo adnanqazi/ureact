dec636f6ff8b330df13279c3dabc4c57798a2868
Adnan qazi <adnanqazi@hotmail.com>
2019-10-23 12:35:41 +1100

    validations added

---
+++ src/main/java/com/aq/ppmtool/services/ProjectService.java
@@ -0,0 +1,25 @@
+package com.aq.ppmtool.services;
+
+import com.aq.ppmtool.domain.Project;
+import com.aq.ppmtool.exceptions.ProjectIdException;
+import com.aq.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
+    }
+
+}
